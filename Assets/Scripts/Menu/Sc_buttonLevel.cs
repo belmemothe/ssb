8f87cee4840c94c9ecd4460cc330194339fa1ded
Lhabitant <loic.lecomte.n.t.a@gmail.com>
2019-09-18 13:49:53 +0200

    Add PopUp
    Add popUp and messenger design pattern in

---
+++ Assets/Scripts/Menu/Sc_buttonLevel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sc_buttonLevel : MonoBehaviour
+{
+    /* Pour plus tards
+    IEnumerator LoadYourAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("SampleScene");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void GotoSampleLevel()
+    {
+        //StartCoroutine(LoadYourAsyncScene());
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}

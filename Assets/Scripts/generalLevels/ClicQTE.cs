848d9456c2594a5820de7ed0411c37112e85539b
PickkPick <55383658+PickkPick@users.noreply.github.com>
2019-09-24 10:22:41 +0200

    JE PUSH DES TRUCS
    BITE
    SINON Y'A QUELQUES UNS DES PNG DE MON NIVEAU
    ET PUIS EVENTUELLEMENT LE DEBUT DU CODE DE QTE

---
+++ Assets/Scripts/generalLevels/ClicQTE.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClicQTE : MonoBehaviour
+{
+    public int TimerQTE = 3;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("oui");
+        }
+    }
+}
